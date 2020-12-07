8d4b7b8b60de23f40ce72ab27c6ea867bdc746b0
Caden <mcaden966@gmail.com>
2020-12-07 16:04:54 -0600

    updated tables

--- pages.js
+++ pages.js
@@ -89,8 +89,10 @@ function createWorkoutPage (week)
 	for (let i = 0; i < week.length; i++)
 	{
 		createH2Tag (week[i].name);
+		let table_container = document.createElement ("div");
+		table_container.id = "table_container" + i;
+		table_container.className = "table_container";
 		let table = document.createElement ("table");
-		table.id = i;
 		let row = table.insertRow (0);
 		let col1 = row.insertCell (0);
 		col1.innerHTML = "BL";
@@ -109,7 +111,8 @@ function createWorkoutPage (week)
 		row.insertCell (1).innerHTML = week[i].HL;
 		row.insertCell (2).innerHTML = week[i].U;
 		row.insertCell (3).innerHTML = week[i].R;
-		document.body.appendChild (table);
+		table_container.appendChild (table);
+		document.body.appendChild (table_container);
 		let hr = document.createElement ("hr");
 		hr.id = "hr" + i;
 		document.body.appendChild (hr);
@@ -125,7 +128,7 @@ function deleteWorkoutPage (week)
 	for (let i = 0; i < week.length; i++)
 	{
 		document.getElementById (week[i].name).remove ();
-		document.getElementById (i).remove ();
+		document.getElementById ("table_container" + i).remove ();
 		document.getElementById ("hr" + i).remove ();
 	}
 }
